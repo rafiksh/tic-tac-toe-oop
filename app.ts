bba04105263a6b93a506b26a24017af1738e621f
Rafik <rafikshmoury@gmail.com>
2022-12-09 09:18:01 +0200

    improve: move initial questions to config

--- app.ts
+++ app.ts
@@ -1,36 +1,11 @@
 import prompts from "prompts";
 
-import { GAME_COMFIG } from "./config";
+import { INITIAL_QUESTIONS } from "./config";
 import { Player } from "./src/player";
 import { Game } from "./src/game";
 
 const start = async () => {
-  const { name, symbol, size } = await prompts([
-    {
-      type: "text",
-      name: "name",
-      message: "What is your name?",
-    },
-    {
-      type: "text",
-      name: "symbol",
-      message: "X/O? X will always start the game.",
-      validate: (symbol) =>
-        !["X", "O"].includes(symbol.toUpperCase())
-          ? `Only X or O are allowed!`
-          : true,
-      format: (symbol) => symbol.toUpperCase(),
-    },
-    {
-      type: "number",
-      name: "size",
-      message: `What do you wnat the size of the board to be?\nSize is between ${GAME_COMFIG.MIN} and ${GAME_COMFIG.MAX}`,
-      validate: (size) =>
-        size < GAME_COMFIG.MIN || size > GAME_COMFIG.MAX
-          ? `Size is between ${GAME_COMFIG.MIN} and ${GAME_COMFIG.MAX}`
-          : true,
-    },
-  ]);
+  const { name, symbol, size } = await prompts(INITIAL_QUESTIONS);
 
   console.log(`Welcome ${name}`);
 
--- config.ts
+++ config.ts
@@ -1,3 +1,32 @@
+import { PromptObject } from "prompts";
+
 const GAME_COMFIG = { MIN: 3, MAX: 10 };
 
-export { GAME_COMFIG };
+const INITIAL_QUESTIONS: PromptObject[] = [
+  {
+    type: "text",
+    name: "name",
+    message: "What is your name?",
+  },
+  {
+    type: "text",
+    name: "symbol",
+    message: "X/O? X will always start the game.",
+    validate: (symbol: string) =>
+      !["X", "O"].includes(symbol.toUpperCase())
+        ? `Only X or O are allowed!`
+        : true,
+    format: (symbol: string) => symbol.toUpperCase(),
+  },
+  {
+    type: "number",
+    name: "size",
+    message: `What do you wnat the size of the board to be?\nSize is between ${GAME_COMFIG.MIN} and ${GAME_COMFIG.MAX}`,
+    validate: (size: string) =>
+      Number(size) < GAME_COMFIG.MIN || Number(size) > GAME_COMFIG.MAX
+        ? `Size is between ${GAME_COMFIG.MIN} and ${GAME_COMFIG.MAX}`
+        : true,
+  },
+];
+
+export { GAME_COMFIG, INITIAL_QUESTIONS };
